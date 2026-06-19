9dd6f40118872104710fc88dfd8bbabd1f23403b
Elect-Trick <snmajenje@gmail.com>
2026-06-19 20:29:11 +0200

    Dependency Injection
    Introduce some DI to the core and infrastructure layer

--- eCommerceAPI/Program.cs
+++ eCommerceAPI/Program.cs
@@ -1,6 +1,19 @@
+using eCommerceInfrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Inject infrastructure services.
+builder.Services.AddInfrastructure();
+builder.Services.AddCoreLayer();
+
+//Add Controllers
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
 
 app.Run();
---
+++ eCommerceInfrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceInfrastructure.DependencyInjection;
+   
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {//Allows us to inject services to the IoC container, Infrastructure services often include data access, caching and other low level components.
+     //TODO
+        return services;
+    }
+}
