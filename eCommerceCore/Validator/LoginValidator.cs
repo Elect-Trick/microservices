be49e4523348f521c29099ebf66cd7b60a01b3e6
Elect-Trick <snmajenje@gmail.com>
2026-06-20 20:44:00 +0200

    Login Validator
    Add Login validator class with basic functionality

---
+++ eCommerceCore/Validator/LoginValidator.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceCore.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceCore.Validator
+{
+    public class LoginValidator: AbstractValidator<LoginDTO>
+    {
+
+        public LoginValidator()
+        {
+
+            RuleFor(obj => obj.Email).NotEmpty().WithMessage("Email Cannot be empty");
+            RuleFor(obj => obj.Password).NotEmpty().WithMessage("Password Cannot be empty");
+            
+        }
+    }
+}
